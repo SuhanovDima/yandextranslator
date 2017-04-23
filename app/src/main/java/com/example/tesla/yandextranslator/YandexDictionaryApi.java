6eed4b0a39effb97c41ef9ccadfca1d7f42aac41
Dmitry Suhanov <dmitry.sukhanov@informicus.ru>
2017-04-23 21:33:16 +0300

    api for values

--- app/src/main/java/com/example/tesla/yandextranslator/YandexDictionaryApi.java
+++ app/src/main/java/com/example/tesla/yandextranslator/YandexDictionaryApi.java
@@ -7,4 +7,9 @@ import retrofit2.http.Query;
 public interface YandexDictionaryApi {
     @GET("/api/v1/dicservice.json/getLangs")
     Call<String[]> getDictionaries(@Query("key") String key);
+
+    @GET("/api/v1/dicservice.json/lookup")
+    Call<String> getLookUp(@Query("key") String key,
+                           @Query("lang") String lang,
+                           @Query("text") String text);
 }
